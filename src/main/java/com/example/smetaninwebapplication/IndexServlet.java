3a8003dbed8b124b79fb338553d5260e1588001b
Dmitriy <88580214+dmitriysmetanin@users.noreply.github.com>
2024-02-07 12:58:08 +0300

    Update IndexServlet.java

--- src/main/java/com/example/smetaninwebapplication/IndexServlet.java
+++ src/main/java/com/example/smetaninwebapplication/IndexServlet.java
@@ -1,18 +1,43 @@
 package com.example.smetaninwebapplication;
 
 import java.io.*;
+import java.util.ArrayList;
 
+import com.example.smetaninwebapplication.studying.dao.CourseDao;
+import com.example.smetaninwebapplication.studying.model.Course;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+@WebServlet("/")
 public class IndexServlet extends HttpServlet {
     private String message;
 
     public void init() {
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
+        Cookie[] cookies = request.getCookies();
+        HttpSession session = request.getSession();
+//
+//        for (Cookie cookie: cookies){
+//                session.setAttribute(cookie.getName(), cookie.getValue());
+//        }
+        session.setAttribute("a", "1");
+        session.setAttribute("b", "2");
+
+        CourseDao courseDao = new CourseDao();
+        try {
+            ArrayList<Course> courses = courseDao.getAllCourses();
+            request.setAttribute("courses", courses);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/WEB-INF/index.jsp");
+        requestDispatcher.forward(request, response);
     }
 
     public void destroy() {
