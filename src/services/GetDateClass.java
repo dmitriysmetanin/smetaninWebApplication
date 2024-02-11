b41a2cd38f0e0e74f4ee4992e33687441671af2d
Dmitriy <88580214+dmitriysmetanin@users.noreply.github.com>
2024-02-11 18:03:38 +0300

    рефакторинг

--- src/services/GetDateClass.java
+++
@@ -1,9 +0,0 @@
-package services;
-import java.util.Date;
-
-public class GetDateClass {
-    public String getCurrentDate(){
-        Date date = new Date();
-        return date.toString();
-    }
-}
