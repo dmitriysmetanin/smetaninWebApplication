01a5884631b26f8164b00f3a9067889ac9815329
Dmitriy <88580214+dmitriysmetanin@users.noreply.github.com>
2024-02-18 22:03:04 +0300

    Удалил лишнее из ModuleDAO

--- src/dao/ModuleDao.java
+++ src/dao/ModuleDao.java
@@ -9,62 +9,7 @@ import java.sql.*;
 import java.util.Properties;
 
 public class ModuleDao {
-    public Hometask getHometask(int module_id) throws SQLException, ClassNotFoundException {
-        if (this.hometaskExists(module_id)){
-            Class.forName("org.postgresql.Driver");
-            String SELECT_HOMETASK = String.format("select * from hometasks where moduleid = '%s'", module_id);
-            String url = "jdbc:postgresql://localhost:5432/smetaninWebApplicationDatabase";
-            Properties props = new Properties();
-            props.setProperty("user", "postgres");
-            props.setProperty("password", "root");
-            props.setProperty("ssl", "false");
-            try (Connection conn = DriverManager.getConnection(url, props);
-                 PreparedStatement preparedStatement = conn.prepareStatement(SELECT_HOMETASK,
-                         ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
-                ResultSet result = preparedStatement.executeQuery();
-                result.first();
-
-                int id = result.getInt("id");
-                String name = result.getString("name");
-                String description = result.getString("description");
-                String content = result.getString("content");
-                int maxPoints = result.getInt("maxPoints");
-                int moduleId = result.getInt("moduleId");
-
-                Hometask hometask = new Hometask();
-                hometask.setId(id);
-                hometask.setName(name);
-                hometask.setDescription(description);
-                hometask.setContent(content);
-                hometask.setMaxPoints(maxPoints);
-                hometask.setModuleId(moduleId);
-
-                return hometask;
-            }
-        } else {
-            return null;
-        }
-    }
-
-    public boolean hometaskExists(int module_id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        String SELECT_MODULE = String.format("select count(*) from hometasks where moduleid = '%s'", module_id);
-        String url = "jdbc:postgresql://localhost:5432/smetaninWebApplicationDatabase";
-        Properties props = new Properties();
-        props.setProperty("user", "postgres");
-        props.setProperty("password", "root");
-        props.setProperty("ssl", "false");
-        try (Connection conn = DriverManager.getConnection(url, props);
-             PreparedStatement preparedStatement = conn.prepareStatement(SELECT_MODULE,
-                     ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
-            ResultSet result = preparedStatement.executeQuery();
-            result.first();
-            int count = result.getInt("count");
-            if (count == 1) {return true;} else {return false;}
-        }
-    }
-
-    public boolean exists(int course_id, int module_id) throws ClassNotFoundException, SQLException{
+    public boolean exists(int course_id, int module_id) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         String SELECT_MODULE = String.format("select count(*) from modules where (id = %s and courseid = %s)", module_id, course_id);
         String url = "jdbc:postgresql://localhost:5432/smetaninWebApplicationDatabase";
@@ -78,7 +23,11 @@ public class ModuleDao {
             ResultSet result = preparedStatement.executeQuery();
             result.first();
             int count = result.getInt("count");
-            if (count == 1) {return true;} else {return false;}
+            if (count == 1) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
@@ -93,7 +42,7 @@ public class ModuleDao {
 
         try (Connection conn = DriverManager.getConnection(url, props);
              PreparedStatement preparedStatement = conn.prepareStatement(SELECT_COURSE,
-                     ResultSet.TYPE_SCROLL_INSENSITIVE,  ResultSet.CONCUR_READ_ONLY)) {
+                     ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
             ResultSet rs = preparedStatement.executeQuery();
             rs.first();
             Module module = new Module();
