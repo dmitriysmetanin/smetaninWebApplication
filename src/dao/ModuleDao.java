e2e66bbe53a7954ea42897f157e6c376d6b669f0
Dmitriy <88580214+dmitriysmetanin@users.noreply.github.com>
2024-02-18 18:30:18 +0300

    Проверка существования домашнего задания для модуля (ModuleDao)

--- src/dao/ModuleDao.java
+++ src/dao/ModuleDao.java
@@ -1,11 +1,32 @@
 package dao;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import models.Module;
 
 import java.sql.*;
 import java.util.Properties;
 
 public class ModuleDao {
+
+    public boolean hometaskExists(int module_id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String SELECT_MODULE = String.format("select count(*) from hometasks where moduleid = '%s'", module_id);
+        String url = "jdbc:postgresql://localhost:5432/smetaninWebApplicationDatabase";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "root");
+        props.setProperty("ssl", "false");
+        try (Connection conn = DriverManager.getConnection(url, props);
+             PreparedStatement preparedStatement = conn.prepareStatement(SELECT_MODULE,
+                     ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            ResultSet result = preparedStatement.executeQuery();
+            result.first();
+            int count = result.getInt("count");
+            if (count == 1) {return true;} else {return false;}
+        }
+    }
+
     public boolean exists(int course_id, int module_id) throws ClassNotFoundException, SQLException{
         Class.forName("org.postgresql.Driver");
         String SELECT_MODULE = String.format("select count(*) from modules where (id = %s and courseid = %s)", module_id, course_id);
