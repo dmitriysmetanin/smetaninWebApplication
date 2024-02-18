0007c1c4e3a46500b7ab4f7613dacdcea9566b74
Dmitriy <88580214+dmitriysmetanin@users.noreply.github.com>
2024-02-18 18:32:45 +0300

    Класс домашнего задания

---
+++ src/models/Hometask.java
@@ -0,0 +1,58 @@
+package models;
+
+public class Hometask {
+    private int id;
+    private String name;
+    private String description;
+    private String content;
+    private int maxPoints;
+    private int moduleId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getMaxPoints() {
+        return maxPoints;
+    }
+
+    public void setMaxPoints(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public int getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(int moduleId) {
+        this.moduleId = moduleId;
+    }
+}
